1d5433c2a3c4a1123239bf959f6be268059b3a9f
jhelsas <jhelsas@gmail.com>
2014-10-20 12:10:24 -0200

    Adicionando cálculo de entropia de gubser analítica

--- src/extra/Gubser-Etest/energy_profile.c
+++ src/extra/Gubser-Etest/energy_profile.c
@@ -9,8 +9,15 @@
 double gubser_transverse_energy(double r,void *p){
   double Q=*(double*)p,foo,lambda;
   foo = r*(3.*(1+Q*Q*(1+r*r))*(1+Q*Q*(1+r*r)) + 4.*Q*Q*Q*Q*r*r);
-  lambda = 1+2.*Q*Q*(1+r*r)+Q*Q*Q*Q*(1-r*r)*(1-r*r);
+  lambda = 1.+2.*Q*Q*(1+r*r)+Q*Q*Q*Q*(1-r*r)*(1-r*r);
   foo = foo/cbrt(gsl_sf_pow_int(lambda,7));
+  return foo;
+}
+
+double gubser_transverse_entropy(double r,void *p){
+  double Q=*(double*)p,foo,lambda;
+  lambda = 1.+2.*Q*Q*(1+r*r)+Q*Q*Q*Q*(1-r*r)*(1-r*r);
+  foo = (r*(1+Q*Q*(1+r*r)*(1+r*r) ) )/sqrt(lambda*lambda*lambda);
   return foo;
 }
 
@@ -19,14 +26,12 @@ int main(){
   size_t limit;
   double tau,tau0,dt,tauM;
   double *p,res,abserr;
-  double eabs,erel,I,q=1.0,Q;
+  double eabs,erel,I,S,q=1.0,Q;
   gsl_integration_workspace * w 
     = gsl_integration_workspace_alloc (1000);
   gsl_function F;
   FILE *dadosout;
-  
-  F.function = &gubser_transverse_energy;
-  
+    
   tau0=1.0;
   tauM=8.0;
   dt=0.01;
@@ -36,10 +41,14 @@ int main(){
   dadosout=fopen("energy_profile.dat","w");
   for(tau=tau0;tau<tauM;tau+=dt){
 	Q=q*tau;
-	F.params = &Q;
+    F.function = &gubser_transverse_energy;
+  	F.params = &Q;
     err=gsl_integration_qagiu(&F,0.0,eabs,erel,limit,w,&I,&abserr);
+    
+    F.function = &gubser_transverse_entropy;
+    err=gsl_integration_qagiu(&F,0.0,eabs,erel,limit,w,&S,&abserr);
 
-    fprintf(dadosout,"%.8f %.8f %.8f %d %lf %lf\n",tau,I,abserr,w->size,log(tau),log(I));
+    fprintf(dadosout,"%.8f %.8f %.8f %.8f %d %lf %lf\n",tau,I,S,abserr,w->size,log(tau),log(I));
   }
   
   fclose(dadosout);
